285e21ca158dbdaf512c32c3c7f8aa3aaea16763
Nathan Trenholm <nathant23@gmail.com>
2016-10-29 23:52:09 -0700

    Updated School_Task_1.R  - Use tidyverse and haven packages, this utilizes tibbles instead of data frames.  - Change from plyr revalue to dplyr recode

--- data/scripts/School_Task_1.R
+++ data/scripts/School_Task_1.R
@@ -17,18 +17,16 @@
 
   setwd("C:/Users/Nathan/Desktop/SDP - Human Capital Toolkit")
   
-  library(foreign)
-  library(plyr)
+  library(haven)  
+  library(tidyverse)
   library(compare)
-  library(readstata13)
 
 
 # Read File & Remove Duplicates -------------------------------------------
 
-  school_raw <- read.dta('data/raw/School_Raw.dta')
+  school_raw <- read_dta('data/raw/School_Raw.dta')
   View(school_raw) 
   nrow(school_raw) == length(unique(school_raw$school_code)) # No duplicate school_codes.
-  View(school_raw[duplicated(school_raw$school_code),])      # No duplicates returned.
 
   
 # Recode and create level columns -----------------------------------------
@@ -39,17 +37,17 @@
   
   # Create columns required for the final clean file with recoded data.
   school_raw$elementary <- ifelse(school_raw$school_lvl == 'Elem', 1,0)
-  school_raw$middle 	  <- ifelse(school_raw$school_lvl == 'Mid' , 1,0)
-  school_raw$high 	    <- ifelse(school_raw$school_lvl == 'High', 1,0)
+  school_raw$middle     <- ifelse(school_raw$school_lvl == 'Mid' , 1,0)
+  school_raw$high       <- ifelse(school_raw$school_lvl == 'High', 1,0)
   
-  school_raw$alternative 	<- as.numeric(revalue(school_raw$alternative, c(No = 0, Yes = 1, . = NA)))
+  school_raw$alternative <- as.numeric(recode(school_raw$alternative, No = '0', Yes = '1', .default = NA_character_))
   
   school_clean <- school_raw[,c('school_code', 'school_name', 'school_lvl', 'elementary', 'middle', 'high', 'alternative')]
 
   
 # Read in clean SDP file to check results. --------------------------------
 
-  sdp_school_clean  <- read.dta13('data/sdp_clean/School_Clean.dta')
+  sdp_school_clean  <- read_dta('data/sdp_clean/School_Clean.dta')
   # Looks like there is an error in the SDP file for the middle school column.  The rest matches.
   compare(school_clean, sdp_school_clean)
   compare(school_clean, sdp_school_clean, allowAll = TRUE)
