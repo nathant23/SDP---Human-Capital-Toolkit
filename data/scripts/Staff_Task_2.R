0af78ff95575ae6277f2a5ae0f24e880014a0d53
Nathan Trenholm <nathant23@gmail.com>
2016-10-31 10:00:21 -0700

    Updating Staff_Task_2 to tidyverse.  - Left off at line 83

--- data/scripts/Staff_Task_2.R
+++ data/scripts/Staff_Task_2.R
@@ -17,48 +17,42 @@
 
 # Set working directory and load packages ---------------------------------
 
-  setwd("C:/Users/Nathan/Desktop/SDP - Human Capital Toolkit")
+  setwd("C:/Users/natha/Desktop/SDP - Human Capital Toolkit")
 
-  library(foreign)
-  library(plyr)
-  library(dplyr)
-  library(lubridate)
-  library(reshape)
   library(compare)
+  library(haven)
+  library(lubridate)
+  library(magrittr)
+  library(tidyverse)
 
 
 # Load raw data -----------------------------------------------------------
 
-  cert_raw <- read.dta('data/raw/staff_certifications.dta')
+  cert_raw <- read_dta('data/raw/staff_certifications.dta')
   View(cert_raw)
  
   
-  
-  
-  
 # Standardize certification values ----------------------------------------
 
-  unique(cert_raw$certification_code)
+  count(cert_raw, certification_code)
   cert_raw$certification_code <- toupper(cert_raw$certification_code)
-  cert_raw$certification_code <- revalue(cert_raw$certification_code,
-                                         c('ESL' 			        = 'ENGLISH AS A SECOND LANGUAGE CERTIFICATION',
-                                           'SPED' 		        = 'SPECIAL EDUCATION CERTIFICATION',
-                                           'NB' 			        = 'NATIONAL BOARD CERTIFICATION',
-                                           'NATL BOARD'       = 'NATIONAL BOARD CERTIFICATION',
-                                           'SPECIAL ED'       = 'SPECIAL EDUCATION CERTIFICATION',
-                                           'ELS' 			        = 'ENGLISH AS A SECOND LANGUAGE CERTIFICATION',
-                                           'BOARD CERTIFIED' 	= 'NATIONAL BOARD CERTIFICATION'))
-  
-  table(cert_raw$certification_code)
-  
-  
+  cert_raw$certification_code <- recode(cert_raw$certification_code,
+                                        'ESL'             = 'ENGLISH AS A SECOND LANGUAGE CERTIFICATION',
+                                        'SPED'            = 'SPECIAL EDUCATION CERTIFICATION',
+                                        'NB'              = 'NATIONAL BOARD CERTIFICATION',
+                                        'NATL BOARD'      = 'NATIONAL BOARD CERTIFICATION',
+                                        'SPECIAL ED'      = 'SPECIAL EDUCATION CERTIFICATION',
+                                        'ELS'             = 'ENGLISH AS A SECOND LANGUAGE CERTIFICATION',
+                                        'BOARD CERTIFIED' = 'NATIONAL BOARD CERTIFICATION')
+  count(cert_raw, certification_code)
+
   
 # Formate Certification Eff. & Exp. Dates ---------------------------------
-  #	- School year will be defined by the year during the Spring                                   ##
-  #	- Certifications effective after May 1 will be valid during the following school year         ##
-  #	- Add one year to eff. dates between Apr. 1 and Dec. 31                                       ##
-  #	- Certifications that expire before Oct. 1 are only valid through the previous school year    ## 
-  #	- Add one year to exp. dates between Oct 1 and Dec 31 to align to the valid school year       ##
+  #	- School year will be defined by the year during the Spring
+  #	- Certifications effective after May 1 will be valid during the following school year
+  #	- Add one year to eff. dates between Apr. 1 and Dec. 31
+  #	- Certifications that expire before Oct. 1 are only valid through the previous school year
+  #	- Add one year to exp. dates between Oct 1 and Dec 31 to align to the valid school year
   #	- Drop records missing a valid effectie year and expiration year
   
   cert_raw$effective_date       <- mdy(cert_raw$effective_date)	
@@ -70,7 +64,7 @@
   cert_raw$expiration_date       <- mdy(cert_raw$expiration_date)
   cert_raw$expiration_date_year  <- year(cert_raw$expiration_date)
   cert_raw$expiration_date_month <- month(cert_raw$expiration_date)
-  cert_raw$expiration_date_year  <- ifelse(cert_raw$expiration_date_month >= 10 & cert_raw$expiration_date_mont <= 12,
+  cert_raw$expiration_date_year  <- ifelse(cert_raw$expiration_date_month >= 10 & cert_raw$expiration_date_month <= 12,
                                            (cert_raw$expiration_date_year + 1), cert_raw$expiration_date_year)
   
   # Drop records where both the effective year and expiration year are null
@@ -81,8 +75,8 @@
   cert_raw$expiration_date_year <- ifelse(is.na(cert_raw$expiration_date_year), cert_raw$effective_date_year, cert_raw$expiration_date_year)
   
   # Drop the month columns they are no longer necessary.
-  # grep('month', names(cert_raw)) returns the column numbers with the word month, putting the negative sign in front excludes those columns.
-  cert_raw   <- cert_raw[,-grep('month', names(cert_raw))]
+  # %<>% operator uses cert_raw as the first argument and then passes the result back to cert_raw.  -contains('month') selects all columns that do not have the word month in the name.
+  cert_raw   %<>% select(-contains('month'))
 
 
   
